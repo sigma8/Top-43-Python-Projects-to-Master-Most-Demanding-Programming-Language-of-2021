62004172f3362dff55ea581a38c6aab3c0a26e6a
jtortolero/gabo <josegabrieltortolero@gmail.com>
2021-11-04 17:58:09 -0300

    Modified alarma.py y se agrego README.md
    Alarma de reloj donde se selecciona hora y se activa mediante checkbox

---
+++ alarma/README.md
@@ -0,0 +1,9 @@
+2. Alarma de reloj Python
+
+Idea de proyecto python: El proposito principal es desarrollar una alarma de reloj.
+
+Datetime y Tkinter son algunas de las librerias mas usadas en python que ayudaran a construir este proyecto.
+
+Para setear la alarma de acuerdo a los requisitos, debe ser amigable con el usuario.
+
+Disclaimer: Pueden ser agragados infinidad de elementos que ayude a ser una alarma mucho mas funcional.
--- alarma/alarma.py
+++ alarma/alarma.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from datetime import datetime
 import time
 
 class Aplicacion():
@@ -7,72 +8,62 @@ class Aplicacion():
         #Aqui creamos lo que va a contener la aplicacion
         #Seteamos tamaño de la ventana y el titulo
         self.root = Tk()
-        self.root.geometry('300x300')
         self.root.title("Alarma")
-
-        
-        
         #Creamos un marco dentro de la ventana que
-        #contiene todos los elementos, hora, botones
-        self.frame = ttk.Frame(self.root, borderwidth=2, relief="raised",
-                               padding = (10,10))
-        self.frame.grid(column=0, row=0, padx=5, pady=5, sticky="nsew")
-        
+        #contiene todos los elementos, hora
+        self.frame = ttk.Frame(self.root)
+        self.frame.pack()
+        #
+        self.frame_bottom = ttk.Frame(self.root)
+        self.frame_bottom.pack()
         #Creamos un marco para contener la hora
         #y la etiqueta que es la que muestra la hora
-        self.fm_hora = ttk.Frame(self.frame, borderwidth=2, padding=(10,10))
-        self.fm_hora.grid(column=1, row=0, sticky="nsew")
-        self.label = ttk.Label(self.fm_hora, text="", font=("times", 30, "bold"))
+        self.label = ttk.Label(self.frame, text="", font=("times", 30, "bold"))
         self.label.pack()
         #llamamos la instancia que contiene la hora actual
         self.reloj()
-        #
-        #
-        self.bt_set = ttk.Button(self.frame, text="Set Alarma", command=self.set_alarma)
-        self.bt_set.grid(column=1, row=6, padx=10, sticky="s")
-        self.bt_out = ttk.Button(self.frame, text="Salir", command=self.root.destroy)
-        self.bt_out.grid(column=1, row=8, pady=10, sticky="s")
-        #
-        self.separador = ttk.Separator(self.frame, orient=HORIZONTAL)
         
-        self.separador.grid(column=0, row=7, pady=10, columnspan=6, sticky="nsew")
+        #el spinbox es para seleccionar horas, minutos y segundos de la alarma, el format=%02.0f
+        #es para mostrar dos digitos siempre, estado=readonly evita poder colocar o modificar
+        #de forma distinta a las flechas subir bajar
         
-        #
+        self.spinbox_hora = Spinbox(self.frame, from_=0, to=23, format="%02.0f",
+                                   width=4, font=('times', 15, 'bold'), state="readonly")
+        self.spinbox_hora.pack(side=LEFT)
+        self.spinbox_minuto = Spinbox(self.frame, from_=0, to=59, format="%02.0f",
+                                     width=4, font=('times', 15, 'bold'), state="readonly")
+        self.spinbox_minuto.pack(side=LEFT)
+        self.spinbox_segundo = Spinbox(self.frame, from_=0, to=59, format="%02.0f",
+                                   width=4, font=('times', 15, 'bold'), state="readonly")
+        self.spinbox_segundo.pack(side=LEFT)
+        self.checkbox_value = BooleanVar(False)
+        self.check = ttk.Checkbutton(self.frame_bottom, variable=self.checkbox_value, command=self.activar_alarma)
+        self.check.pack(side=LEFT)    
+        self.labelcheck = ttk.Label(self.frame_bottom, text="Activar Alarma", font=("times",10))
+        self.labelcheck.pack(side=LEFT)
         self.root.mainloop()
-
+        
+    #obtener la hora actual
     def reloj(self):
-        now = time.strftime("%H:%M:%S %p")
+        now = datetime.now().strftime("%H:%M:%S")
         self.label.configure(text=now)
         self.root.after(1000, self.reloj)
 
-    def set_alarma(self):
-        self.ventana = Toplevel(self.root)
-        self.ventana.geometry("400x100")
-        self.ventana.title("Set Alarma")
-        self.fm_set_alarma = ttk.Frame(self.ventana, borderwidth=2, padding=(10,10))
-        self.fm_set_alarma.grid(column=1, row=0, sticky="nsew")
-        
-        self.spinbox_hora = Spinbox(self.fm_set_alarma, from_=1, to=12, format="%02.0f",
-                                   width=4, font=('times', 20, 'bold'), state="readonly")
-        self.spinbox_hora.grid(column=1, row=2, padx=10, sticky="w")
-        self.spinbox_minuto = Spinbox(self.fm_set_alarma, from_=0, to=59, format="%02.0f",
-                                     width=4,font=('times', 20, 'bold'), state="readonly")
-        self.spinbox_minuto.grid(column=2, row=2, padx=10, sticky="e")
-        self.spinbox_ampm = Spinbox(self.fm_set_alarma, values=("AM","PM"),
-                                    width=4,font=('times', 20, 'bold'), state="readonly")
-        self.spinbox_ampm.grid(column=3, row=2, padx=10, sticky="w")
-        self.bt_alarma = ttk.Button(self.fm_set_alarma, text= "Set", command=self.button_set)
-        self.bt_alarma.grid(column=1, row=6, padx=10, pady=10, sticky="s")
-        self.bt_cancelar = ttk.Button(self.fm_set_alarma, text="Cerrar",
-                                     command=self.ventana.destroy)
-        self.bt_cancelar.grid(column=2, row=6, padx=10, pady=10, sticky="s")
-        
-    def button_set(self):
-        despertador = [self.spinbox_hora.get(), self.spinbox_minuto.get(), self.spinbox_ampm.get()]
-        self.label_alarma = ttk.Label(self.fm_hora, text="", font=("times", 30, "bold"))
-        self.label_alarma.configure(text= f'{despertador[0]}:{despertador[1]} {despertador[2]}')
-        self.label_alarma.pack()
+    #alarma activa, despues de seleccionar checkbox cuenta cada segundo hasta que llegue a la hora 
+    def activar_alarma(self):
+        while self.checkbox_value.get()==True:
+            time.sleep(1)
+            despertador = f"{self.spinbox_hora.get()}:{self.spinbox_minuto.get()}:00"
+            hora_actual = datetime.now().strftime("%H:%M:%S")
+            #print(despertador, hora_actual)
+            if despertador == hora_actual:
+                print("Hola vietnant")
+                break
 
+        
+
+#inicializa la aplicacion
+        
 def main():
     mi_app = Aplicacion()
     return 0
