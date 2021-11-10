f4106dcf3b57aff1a48d61bb65ca62bf1d323f38
jtortolero <josegabrieltortolero@gmail.com>
2021-11-10 17:59:02 -0300

    creado notifier.py, README.md, requirements.txt

---
+++ Notifier/README.md
@@ -0,0 +1,9 @@
+4. Notificador de escritorio Python
+
+Idea de proyecto Python: Crear un notificador de escritorio que te notificará despuesde cierta cantidad de tiempo.
+
+La aplicacion funciona en tu sistema.
+
+El desarrollo de esta aplicacion podrar hacer uso de las librerias requests, notify2, etc.
+
+Disclaimer: No se uso notify2 se usó notifypy, no corre en el sistema ni se ejecuta cierta cantidad de tiempo.
--- Notifier/notifier.py
+++ Notifier/notifier.py
@@ -1,8 +1,36 @@
 from notifypy import Notify
+import requests
+from bs4 import BeautifulSoup
 
+
+#requets a la pagina de github para obtener los datos de los repositorios
+r = requests.get('https://github.com/sigma8?tab=repositories').text
+soup = BeautifulSoup(r, 'lxml')
+
+#con esta funcion obtengo el primer repositorio
+def repositorio(bsdatos): # type bsdatos: class 'bs4.element.Tag'
+    titulo = bsdatos.h3.a.text.strip()
+    descripcion = bsdatos.p.text.strip()
+    fecha = bsdatos.find('relative-time').text.strip()
+    datos = [titulo, descripcion, fecha]
+    return datos # type datos: list[str]
+#con esta funcion obtengo todos los repositorios
+def repositorios_total():
+    informacion =[]
+    for elemento in soup.find_all('li', class_="public"):
+        resultado = repositorio(elemento)
+        informacion.append(resultado)
+    return informacion # type informacion: list[list[str]]
+
+#variable para almacenar la lista de los repositorios
+repo = repositorios_total()
+#aqui determino cuantos repositorios existen
+total_repos = len(repo)
+
+#crear y enviar la notificacion
 notificacion = Notify()
-notificacion.tittle = "Titulo"
-notificacion.message = "mensaje"
+notificacion.tittle = "Repositorios github"
+notificacion.message = f"Los repositorios son {total_repos}, y  el ultimo repositorio actualizado es: {repo[0][0]}, fecha: {repo[0][2]}"
 
 notificacion.send(block=False)
 
