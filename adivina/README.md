799a12c36bf933dc148b9fe75540aa29946be774
jtortolero <josegabrieltortolero@gmail.com>
2021-11-12 11:55:51 -0300

    adivina.py es modificado, README.md y requirements.txt es agregado

---
+++ adivina/README.md
@@ -0,0 +1,7 @@
+6. Adivina el numero en Python.
+
+Idea de proyecto Python: Este es uno de los proyectos mas exitantes cuyo obejtivo es desarrollar un mini juego.
+
+En este programa, la computadora de forma aleatoria escoje un numero luego, luego el usuario debe identificarlo usando la pista.
+
+La puntuacion del usuario se reduce cada vez que este(a) da la respuesta incorrecta y aumenta si acierta.
--- adivina/adivina.py
+++ adivina/adivina.py
@@ -7,15 +7,18 @@ chance = 0
 
 print('BIENVENIDO AL JUEGO: ADIVINAR EL NUMERO!!!')
 time.sleep(2)
-print("Las reglas son muy sencillas")
+print("Las reglas son muy sencillas...")
 time.sleep(2)
-print("1. El numero a elegir debe estar entre el numero 1 y el numero 99 incluyendo a ambos")
-time.sleep(1)
-print("2. Tienes tres oportunidades para adivinar el numero")
-time.sleep(1)
-print("3. Si el numero que eliges es incorrecto perderas 10 puntos, y si aciertas ganaras 10 puntos")
-time.sleep(1)
+print("1. El numero a elegir debe estar entre el numero 1 y el numero 99 incluyendo a ambos.")
+time.sleep(2)
+print("2. Tienes tres oportunidades para adivinar el numero.")
+time.sleep(2)
+print("3. Si el numero que eliges es incorrecto perderas 10 puntos, y si aciertas ganaras 10 puntos.")
+time.sleep(2)
 print("4. Los puntos son acumulados hasta que decidas terminar de jugar.")
+time.sleep(2)
+print("Y no te preocupes, recibiras una ayuda antes de cada intento.")
+
 time.sleep(3)
 print("A jugar!!!")
 
@@ -59,27 +62,38 @@ def pistas(n, i):
         ayuda = f"Aqui va la ayuda numero {i+1}: {pista_tres}"
     return ayuda
 
-
+time.sleep(2)
 while True:
     num_maquina = random.randint(1, 99)
     while chance < 3:
+        time.sleep(2)
         print(pistas(num_maquina, chance))
-        time.sleep(1)
-        adivinar = int(input("Intenta adivinar, di un numero: "))
-        
-        if num_maquina != adivinar:
-            chance += 1
-            p = p - 10
-            print(f"no acertaste, tu puntuacion es: {p}")
-        else:
-            p = p + 10
-            print(f"acertaste, tu puntuacion es: {p}")
-            break
+        time.sleep(2)
+        try:
+            adivinar = int(input("Intenta adivinar, di un numero: "))
+            if num_maquina != adivinar:
+                chance += 1
+                p = p - 10
+                print(f"no acertaste, tu puntuacion es: {p}")
+            else:
+                p = p + 10
+                print(f"acertaste, tu puntuacion es: {p}")
+                break
+        except ValueError:
+            print("Debes usar un numero")
+    time.sleep(2)        
     print(f"el numero era: {num_maquina}")
     time.sleep(2)
-    seguir = input("Deseas seguir jugando si/no: ")
-    if seguir == "no":
-        print(f"Tu puntuacion final es: {p}")
-        break
-    else:
-        chance = 0
+
+    try:
+        seguir = str(input("Deseas seguir jugando si/no: ").lower())
+        if seguir == "si":
+            chance = 0
+        elif seguir == "no":
+            print(f"Tu puntuacion final es: {p}")
+            break
+        else:
+            print("No es una opcion valida debe responder, si o no!!!")
+    except ValueError:
+        print("No es una opcion valida debe responder, si o no!!!")
+    
